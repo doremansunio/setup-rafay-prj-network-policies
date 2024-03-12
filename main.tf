ebf68088585b3506a0c4570cb815070ee5157680
Phani <Phani@gmail.com>
2024-03-12 13:29:32 +0530

    kk1

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ provider "github" {
 
 resource "rafay_namespace_network_policy_rule" "demo-withinworkspacerule" {  
   metadata {    
-    name    = var.network_policy_rule_name
+    name    = var.within_ws_ntwrk_rule_name
     project = var.project_name    
   }
   spec {
@@ -40,16 +40,55 @@ resource "rafay_namespace_network_policy_rule" "demo-withinworkspacerule" {
   }
 }
 
+resource "rafay_namespace_network_policy_rule" "demo-denyallns-rule" {  
+  metadata {    
+    name    = var.denyall_ns_ntwrk_rule_name
+    project = var.project_name    
+  }
+  spec {
+    artifact {
+      type = "Yaml"
+      artifact {    
+        paths {                               
+          name = "file://${path.module}/netfiles/${var.project_name}-deny-all-ns-rule.yaml"        
+        } 
+      }
+    }
+    version = var.network_policy_rule_version
+    sharing {
+      enabled = false
+    }
+  }
+}
+
 resource "rafay_namespace_network_policy" "demo-withinworkspacepolicy" {
   depends_on = [rafay_namespace_network_policy_rule.demo-withinworkspacerule]
   metadata {
-    name    = var.network_policy_name
+    name    = var.within_ws_ntwrk_policy_name
     project = var.project_name
   }
   spec {
     version = var.network_policy_rule_version
     rules {
-      name = var.network_policy_rule_name
+      name = var.within_ws_ntwrk_rule_name
+      version = var.network_policy_rule_version
+    }
+    sharing {
+      enabled = false
+    }
+  }
+}
+
+resource "rafay_namespace_network_policy" "demo-denyallns-policy" {
+  depends_on = [rafay_namespace_network_policy_rule.demo-withinworkspacerule]
+  metadata {
+    name    = var.denyall_ns_ntwrk_policy_name
+    project = var.project_name
+  }
+  spec {
+    version = var.network_policy_rule_version
+    rules {
+      name = var.denyall_ns_ntwrk_rule_name
       version = var.network_policy_rule_version
     }
     sharing {
