8471d3924bac2aaf1c0016ea65ea6ba44d372611
Phani <Phani@gmail.com>
2024-03-12 14:11:58 +0530

    g2

--- main.tf
+++ main.tf
@@ -40,7 +40,26 @@ resource "rafay_namespace_network_policy_rule" "demo-withinworkspacerule" {
   }
 }
 
+resource "rafay_namespace_network_policy" "demo-withinworkspacepolicy" {
+  depends_on = [rafay_namespace_network_policy_rule.demo-withinworkspacerule]
+  metadata {
+    name    = var.within_ws_ntwrk_policy_name
+    project = var.project_name
+  }
+  spec {
+    version = var.network_policy_rule_version
+    rules {
+      name = var.within_ws_ntwrk_policy_rule_name
+      version = var.network_policy_rule_version
+    }
+    sharing {
+      enabled = false
+    }
+  }
+}
+
 resource "rafay_namespace_network_policy_rule" "demo-denyallns-rule" {  
+  depends_on = [rafay_namespace_network_policy.demo-withinworkspacepolicy]
   metadata {    
     name    = var.denyall_ns_ntwrk_rule_name
     project = var.project_name    
@@ -61,26 +80,8 @@ resource "rafay_namespace_network_policy_rule" "demo-denyallns-rule" {
   }
 }
 
-resource "rafay_namespace_network_policy" "demo-withinworkspacepolicy" {
-  depends_on = [rafay_namespace_network_policy_rule.demo-withinworkspacerule]
-  metadata {
-    name    = var.within_ws_ntwrk_policy_name
-    project = var.project_name
-  }
-  spec {
-    version = var.network_policy_rule_version
-    rules {
-      name = var.within_ws_ntwrk_policy_rule_name
-      version = var.network_policy_rule_version
-    }
-    sharing {
-      enabled = false
-    }
-  }
-}
-
 resource "rafay_namespace_network_policy" "demo-denyallns-policy" {
-  depends_on = [rafay_namespace_network_policy_rule.demo-withinworkspacerule]
+  depends_on = [rafay_namespace_network_policy_rule.demo-denyallns-rule]
   metadata {
     name    = var.denyall_ns_ntwrk_policy_name
     project = var.project_name
